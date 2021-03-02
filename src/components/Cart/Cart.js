9c4116b4e8b49889f2b424604a43128a9d84a73b
Md. Najmul Huda Hridoy <hridoy.3000plus@gmail.com>
2021-03-03 05:45:15 +0600

    practice simple react project

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Cart = (props) => {
+  const cart=props.cart;
+  //const total=cart.reduce((total,prd) => total + prd.price, 0)
+  let total=0;
+  for(let i=0; i<cart.length; i++){
+    total += cart[i].price;
+  }
+  return (
+    <div>
+      <h4>Order Summery</h4>
+      <p>Item oredered: {cart.length}</p>
+      <p>Total price: {total}</p>
+    </div>
+  );
+};
+
+export default Cart;
